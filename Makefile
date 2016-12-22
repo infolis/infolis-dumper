616ea04d2bcce215d25a63b0319be43a92f4924c
Konstantin Baierer <unixprog@gmail.com>
2016-12-22 14:06:22 +0100

    .

--- Makefile
+++ Makefile
@@ -1,5 +1,13 @@
-db.json:
-	curl 'http://infolis.gesis.org/infolink/search?regex=on&id=.*&type=dataset' > $@
+%.json:
+	curl 'http://infolis.gesis.org/infolink/search?regex=on&id=.*&type=$(basename $@)' > $@.tmp && mv $@.tmp $@
 
-db.csv:
-	./create.js csv > $@
+%.csv: %.json
+	./create.js < $< > $@.tmp && mv $@.tmp $@
+
+%.clean:
+	rm -f *.$(basename $@)
+	rm -f *.tmp
+
+clean: json.clean csv.clean
+
+all: publication.csv dataset.csv
--- create.js
+++ create.js
@@ -1,42 +1,43 @@
-#!env node
+#!/usr/bin/env node
 
 const fs = require('fs');
 const args = process.argv.slice(2)
+
+const INNERSEP = ';'
+const SEP = ','
 
 function usage() {
     console.log(`${process.argv[1]} csv`)
 }
 
 function _quote(s) {
+    if (Array.isArray(s)) s = s.join(INNERSEP);
     return '"' + s.replace('"', '\"') + '"'
 }
 
 const csvKeys = [
-    ['from_identifiers', link => link.fromEntity.identifiers[0]],
-    ['from_title',       link => link.fromEntity.name],
     ['from_type',        link => link.fromEntity.entityType],
-    ['to_identifiers',   link => link.toEntity.identifiers[0]],
-    ['to_title',         link => link.toEntity.name],
+    ['from_identifiers', link => link.fromEntity.identifiers],
     ['to_type',          link => link.toEntity.entityType],
+    ['to_identifiers',   link => link.toEntity.identifiers],
+    ['from_title',       link => link.fromEntity.name],
+    ['to_title',         link => link.toEntity.name],
+    ['relation',         link => link.entityRelations],
 ]
 
 
-if (args[0] === 'csv') {
-    fs.readFile('db.json', {encoding:'utf8'}, (err, data) => {
-        if (err) {
-            if (err.code === 'ENOENT') {
-                console.error("ERROR: Run 'make db.json' to download the JSON dump")
-                process.exit(1)
-            } else {
-                console.error(err)
-                process.exit(2)
-            }
-        }
-        console.log(csvKeys.map(csvDef => csvDef[0]).join(','))
-        JSON.parse(data).forEach(link => {
-            console.log(csvKeys.map(csvDef => _quote(csvDef[1](link))).join(','))
-        })
+let jsonStr = ''
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+    let chunk;
+    while (chunk = process.stdin.read()) jsonStr += chunk;
+})
+process.stdin.on('end', () => {
+    // header
+    console.log(csvKeys.map(csvDef => csvDef[0]).join(','))
+    // rows
+    JSON.parse(jsonStr).forEach(link => {
+        // console.error(link)
+        console.log(csvKeys.map(csvDef => _quote(csvDef[1](link))).join(','))
     })
-} else {
-    usage()
-}
+})
